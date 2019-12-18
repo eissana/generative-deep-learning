927b247398ac97d4bd9e9a81c2944a17ea092ca4
Eissa Nematollahi <eissana@gmail.com>
2019-12-18 00:44:23 -0500

    add evaluator, save/load model

--- app/chapter02/deep_neural_network.py
+++ app/chapter02/deep_neural_network.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras.utils import to_categorical
 from keras.datasets import cifar10
 from keras.layers import Input, Flatten, Dense
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.optimizers import Adam
 
 
@@ -21,38 +21,33 @@ def load_cifar10():
 
     return (train_x, train_y), (test_x, test_y)
 
-class NeuralNetwork(object):
-    def __init__(self):
-        self.__model = None
+def model(input_shape, num_classes, learning_rate):
+    input_layer = Input(shape=input_shape)
+    x = Flatten()(input_layer)
+    x = Dense(units=200, activation='relu')(x)
+    x = Dense(units=150, activation='relu')(x)
+    output_layer = Dense(units=num_classes, activation='softmax')(x)
 
-    def model(self, input_shape, num_classes, learning_rate):
-        if self.__model is None:
-            input_layer = Input(shape=input_shape)
-            x = Flatten()(input_layer)
-            x = Dense(units=200, activation='relu')(x)
-            x = Dense(units=150, activation='relu')(x)
-            output_layer = Dense(units=num_classes, activation='softmax')(x)
-
-            self.__model = Model(input_layer, output_layer)
-            self.__model.compile(
-                loss='categorical_crossentropy', 
-                optimizer=Adam(lr=learning_rate),
-                metrics=['accuracy'],
-            )
-        return self.__model
-
-    def fit(self, **args):
-        self.model().fit(**args)
-
+    m = Model(input_layer, output_layer)
+    m.compile(
+        loss='categorical_crossentropy', 
+        optimizer=Adam(lr=learning_rate),
+        metrics=['accuracy'],
+    )
+    return m
 
 if __name__ == "__main__":
-    (train_x, train_y), (test_x, test_y) = load_cifar10()
-    print(train_x.shape)
-    print(train_y.shape)
-    print(test_x.shape)
-    print(test_y.shape)
+    import os.path
 
-    net = NeuralNetwork()
-    model = net.model(input_shape=train_x.shape[1:], num_classes=train_y.shape[1], learning_rate=0.01)
-    model.summary()
-    model.fit(train_x, train_y, batch_size=32, epochs=10, shuffle=True)
+    model_file = "models/nn.h5"
+    (train_x, train_y), (test_x, test_y) = load_cifar10()
+
+    if os.path.isfile(model_file):
+        model = load_model(model_file)
+    else:
+        model = model(input_shape=train_x.shape[1:], num_classes=train_y.shape[1], learning_rate=0.0005)
+        model.summary()
+        model.fit(train_x, train_y, batch_size=32, epochs=10, shuffle=True)
+        model.save(model_file)
+
+    model.evaluate(test_x, test_y)
