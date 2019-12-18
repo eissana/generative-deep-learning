f7f32de191ed0cb75e4778fbf13728103bd024da
Eissa Nematollahi <eissana@gmail.com>
2019-12-18 00:17:41 -0500

    add fit method

--- app/chapter02/deep_neural_network.py
+++ app/chapter02/deep_neural_network.py
@@ -22,27 +22,28 @@ def load_cifar10():
     return (train_x, train_y), (test_x, test_y)
 
 class NeuralNetwork(object):
-    def __init__(self, input_shape, num_classes, learning_rate):
-        self.__input_shape = input_shape
-        self.__num_out_nodes = num_classes
-        self.__learning_rate = learning_rate
-
+    def __init__(self):
         self.__model = None
 
-    def build_model(self):
-        input_layer = Input(shape=self.__input_shape)
-        x = Flatten()(input_layer)
-        x = Dense(units=200, activation='relu')(x)
-        x = Dense(units=150, activation='relu')(x)
-        output_layer = Dense(units=self.__num_out_nodes, activation='softmax')(x)
+    def model(self, input_shape, num_classes, learning_rate):
+        if self.__model is None:
+            input_layer = Input(shape=input_shape)
+            x = Flatten()(input_layer)
+            x = Dense(units=200, activation='relu')(x)
+            x = Dense(units=150, activation='relu')(x)
+            output_layer = Dense(units=num_classes, activation='softmax')(x)
 
-        self.__model = Model(input_layer, output_layer)
-        self.__model.compile(
-            loss='categorical_crossentropy', 
-            optimizer=Adam(lr=self.__learning_rate),
-            metrics=['accuracy'],
-        )
+            self.__model = Model(input_layer, output_layer)
+            self.__model.compile(
+                loss='categorical_crossentropy', 
+                optimizer=Adam(lr=learning_rate),
+                metrics=['accuracy'],
+            )
         return self.__model
+
+    def fit(self, **args):
+        self.model().fit(**args)
+
 
 if __name__ == "__main__":
     (train_x, train_y), (test_x, test_y) = load_cifar10()
@@ -51,6 +52,7 @@ if __name__ == "__main__":
     print(test_x.shape)
     print(test_y.shape)
 
-    net = NeuralNetwork(input_shape=train_x.shape[1:], num_classes=train_y.shape[1], learning_rate=0.01)
-    model = net.build_model()
+    net = NeuralNetwork()
+    model = net.model(input_shape=train_x.shape[1:], num_classes=train_y.shape[1], learning_rate=0.01)
     model.summary()
+    model.fit(train_x, train_y, batch_size=32, epochs=10, shuffle=True)
