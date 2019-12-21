e1ddc6e85a7e6f300a48e9e6867f6606511edbb2
Eissa Nematollahi <eissana@gmail.com>
2019-12-21 00:58:12 -0500

    add image reconstruct func

--- app/chapter03/autoencoder.py
+++ app/chapter03/autoencoder.py
@@ -22,31 +22,34 @@ class AutoencoderModel(object):
 
         self.__encoder_input_layer = None
         self.__encoder_output_layer = None
+        self.__encoder = None
 
         self.__decoder_input_layer = None
         self.__decoder_output_layer = None
+        self.__decoder = None
+
+        self.__model = None
 
         self.__use_batch_norm = use_batch_norm
         self.__use_dropout = use_dropout
 
+    def fit(self, x, *args):
+        return self.model().fit(x=x, y=x, *args)
+
     def model(self):
-        if self.__encoder_input_layer is None:
-            self.__encoder_layers()
+        if self.__model is None:
+            if self.__encoder_input_layer is None:
+                self.__encoder_layers()
 
-        if self.__decoder_input_layer is None:
-            self.__decoder_layers()
+            input_layer = self.__encoder_input_layer
+            output_layer = self.decoder()(self.__encoder_output_layer)
 
-        decoder_model = Model(self.__decoder_input_layer, self.__decoder_output_layer)
-
-        input_layer = self.__encoder_input_layer
-        output_layer = decoder_model(self.__encoder_output_layer)
-
-        m = Model(input_layer, output_layer)
-        m.compile(
-            loss=lambda x, y: K.mean(K.square(x - y), axis = [1,2,3]), 
-            optimizer=Adam(lr=self.__learning_rate),
-        )
-        return m
+            self.__model = Model(input_layer, output_layer)
+            self.__model.compile(
+                loss=lambda x, y: K.mean(K.square(x - y), axis = [1,2,3]), 
+                optimizer=Adam(lr=self.__learning_rate),
+            )
+        return self.__model
 
     def plot_model(self):
         from app.__init__ import MODELS_PLOT_DIR
@@ -57,22 +60,24 @@ class AutoencoderModel(object):
             makedirs(filepath)
 
         plot_model(self.model(), to_file=path.join(filepath ,'model.png'), show_shapes = True, show_layer_names = True)
-        plot_model(self.encoder_model(), to_file=path.join(filepath ,'encoder.png'), show_shapes = True, show_layer_names = True)
-        plot_model(self.decoder_model(), to_file=path.join(filepath ,'decoder.png'), show_shapes = True, show_layer_names = True)
+        plot_model(self.encoder(), to_file=path.join(filepath ,'encoder.png'), show_shapes = True, show_layer_names = True)
+        plot_model(self.decoder(), to_file=path.join(filepath ,'decoder.png'), show_shapes = True, show_layer_names = True)
 
-    def encoder_model(self):
-        if self.__encoder_input_layer is None:
-            self.__encoder_layers()
+    def encoder(self):
+        if self.__encoder is None:
+            if self.__encoder_input_layer is None:
+                self.__encoder_layers()
 
-        m = Model(self.__encoder_input_layer, self.__encoder_output_layer)
-        return m
+            self.__encoder = Model(self.__encoder_input_layer, self.__encoder_output_layer)
+        return self.__encoder
 
-    def decoder_model(self):
-        if self.__decoder_input_layer is None:
-            self.__decoder_layers()
+    def decoder(self):
+        if self.__decoder is None:
+            if self.__decoder_input_layer is None:
+                self.__decoder_layers()
 
-        m = Model(self.__decoder_input_layer, self.__decoder_output_layer)
-        return m
+            self.__decoder = Model(self.__decoder_input_layer, self.__decoder_output_layer)
+        return self.__decoder
 
     def __encoder_layers(self):
         self.__encoder_input_layer = Input(shape=self.__input_shape, name='encoder_input')
@@ -114,6 +119,32 @@ class AutoencoderModel(object):
             x = Dropout(rate = 0.25)(x)
         return x
 
+
+    def reconstruct_images(self, num_show, test_x)
+        example_idx = np.random.choice(range(len(test_x)), num_show)
+        example_images = test_x[example_idx]
+
+        z_points = self.encoder().predict(example_images)
+
+        reconst_images = self.decoder().predict(z_points)
+
+        fig = plt.figure(figsize=(15, 3))
+        fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+        for i in range(num_show):
+            img = example_images[i].squeeze()
+            ax = fig.add_subplot(2, num_show, i+1)
+            ax.axis('off')
+            ax.text(0.5, -0.35, str(np.round(z_points[i],1)), fontsize=10, ha='center', transform=ax.transAxes)   
+            ax.imshow(img, cmap='gray_r')
+
+        for i in range(num_show):
+            img = reconst_images[i].squeeze()
+            ax = fig.add_subplot(2, num_show, i+num_show+1)
+            ax.axis('off')
+            ax.imshow(img, cmap='gray_r')
+
+
 if __name__ == "__main__":
     from app.__init__ import MODELS_DIR
 
@@ -125,18 +156,19 @@ if __name__ == "__main__":
     (train_x, train_y), (test_x, test_y) = load_mnist()
 
     if path.isfile(model_file):
-        model = load_model(model_file)
+        with open(model_file, 'rb') as f:
+            ae = pickle.load(f)
     else:
         ae = AutoencoderModel(
             input_shape=train_x.shape[1:], 
             learning_rate=0.0005,
             use_batch_norm=False,
             use_dropout=False)
-        model = ae.model()
-        model.summary()
-        model.fit(train_x[:1000], train_x[:1000], batch_size=32, epochs=2, shuffle=True)
-        model.save(model_file)
+        ae.fit(x=train_x[:1000], batch_size=32, epochs=2, shuffle=True)
 
+        with open(model_file, 'wb') as f:
+            pickle.dump(ae, f)
+
+    ae.model().summary()
     ae.plot_model()
-
-
+    ae.reconst_images(num_show=10, test_x)
